def136091e08eecd6d004131752e36dd9391295a
Aleksandr But <but.aleksandr@apriorit.com>
2018-09-05 16:50:03 +0300

    Implemented displaying of the heroes list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -10,11 +11,17 @@ export class HeroesComponent implements OnInit {
   hero: Hero = {
     id: 1,
     name: 'Some hero 1'
-  }
+  };
+  heroes = HEROES;
+  selectedHero: Hero;
 
   constructor() { }
 
   ngOnInit() {
+    this.selectedHero = this.heroes[0];
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
